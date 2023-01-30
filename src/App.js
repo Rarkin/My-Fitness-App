86684c47925876cf0d95199387d57fb22962590e
Karl <karlgraves28@gmail.com>
2023-01-30 13:04:44 +1100

    added pages and components to be filled out

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import {Box} from '@mui/material';
+
 import './App.css';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>App</div>
+    <Box width='400px'>
+        Navbar
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/exercise/:id' element={<ExerciseDetail />} />
+        </Routes>
+        <Footer />
+    </Box>
   )
 }
 
